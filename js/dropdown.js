cf1b8ac081186c2aca7a8c035b7bed0ee1fe1c03
rephael123 <202310684@fit.edu.ph>
2024-07-19 23:26:38 +0800

    Add files via upload

---
+++ js/dropdown.js
@@ -0,0 +1,8 @@
+function toggleDropdown(id) {
+  const dropdown = document.getElementById(id);
+  if (dropdown.style.display === "none" || dropdown.style.display === "") {
+    dropdown.style.display = "block";
+  } else {
+    dropdown.style.display = "none";
+  }
+}
